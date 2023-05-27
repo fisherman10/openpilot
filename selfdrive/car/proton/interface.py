a8e22111bd7d7616781f144a405e5c512b8ab019
fisherman10 <fisherman10@gmail.com>
2023-05-27 14:37:45 +0800

    Update interface.py

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -27,7 +27,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerRateCost = 0.7                # Lateral MPC cost on steering rate, higher value = sharper turn
     ret.steerLimitTimer = 0.1              # time before steerLimitAlert is issued
     ret.steerControlType = car.CarParams.SteerControlType.torque
-    ret.steerActuatorDelay = 0.25          # Steering wheel actuator delay in seconds
+    ret.steerActuatorDelay = 0.20          # Steering wheel actuator delay in seconds
 
     ret.lateralTuning.init('pid')
    
@@ -48,7 +48,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV = [0.05, 0.15, 0.15, 0.15]
       ret.lateralTuning.pid.kiBP = [0., 20., 30.]
       ret.lateralTuning.pid.kiV = [0.12, 0.20, 0.40]
-      ret.lateralTuning.pid.kf = 0.000142
+      ret.lateralTuning.pid.kf = 0.000145
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
       ret.longitudinalTuning.kpV = [0, 0, 0]
