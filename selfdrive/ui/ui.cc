15a5e5c99e2e4ff54678c0d879634693254bd77c
fisherman10 <fisherman10@gmail.com>
2023-08-04 23:50:13 +0800

    Update ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -340,12 +340,12 @@ else {
 
 
 
-#  if (brightness != last_brightness) {
-#   if (!brightness_future.isRunning()) {
-#      brightness_future = QtConcurrent::run(Hardware::set_brightness, brightness);
-#      last_brightness = brightness;
-#   }
-#  }
+  if (brightness != last_brightness) {
+   if (!brightness_future.isRunning()) {
+      brightness_future = QtConcurrent::run(Hardware::set_brightness, brightness);
+      last_brightness = brightness;
+   }
+  }
 }
 
 bool Device::motionTriggered(const UIState &s) {
