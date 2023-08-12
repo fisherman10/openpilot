7ef02298c0f1cfe7c2a745102705c8f5a062527d
fisherman10 <fisherman10@gmail.com>
2023-08-12 22:37:37 +0800

    Update ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -331,7 +331,7 @@ void Device::resetInteractiveTimout() {
   // }
 
   // here is where the m4gic happens, tune at your taste and enjoy your day!!
-  if (timeinfo.tm_hour > hour_to_begin_dim && timeinfo.tm_hour < hour_to_revert_dim) {
+  if (timeinfo.tm_hour > hour_to_begin_dim || timeinfo.tm_hour < hour_to_revert_dim) {
      Hardware::set_brightness(10);
 }
  else {
