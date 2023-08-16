e8f02b91f8a89d702bc1d9d1d23f39bf1589b94a
fisherman10 <fisherman10@gmail.com>
2023-08-16 20:15:05 +0800

    Update ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -283,17 +283,24 @@ void Device::resetInteractiveTimout() {
   float clipped_brightness = BACKLIGHT_OFFROAD;
   if (s.scene.started) {
     // Scale to 0% to 100%
+	  if (((timeinfo.tm_hour >= 18) && (timeinfo.tm_hour <= 24)) || ((timeinfo.tm_hour >=0) && (timeinfo.tm_hour <= 8))) {
+     clipped_brightness = 10;
+}
+ else {
+     clipped_brightness = 95;
+  }
+
     clipped_brightness = s.scene.light_sensor;
 
     // CIE 1931 - https://www.photonstophotos.net/GeneralTopics/Exposure/Psychometric_Lightness_and_Gamma.htm
-    if (clipped_brightness <= 8) {
-      clipped_brightness = (clipped_brightness / 903.3);
-    } else {
-      clipped_brightness = std::pow((clipped_brightness + 16.0) / 116.0, 3.0);
-    }
+//    if (clipped_brightness <= 8) {
+//      clipped_brightness = (clipped_brightness / 903.3);
+//    } else {
+//      clipped_brightness = std::pow((clipped_brightness + 16.0) / 116.0, 3.0);
+//    }
 
     // Scale back to 10% to 100%
-    clipped_brightness = std::clamp(100.0f * clipped_brightness, 10.0f, 100.0f);
+//    clipped_brightness = std::clamp(100.0f * clipped_brightness, 10.0f, 100.0f);
   }
 
   int brightness = brightness_filter.update(clipped_brightness);
@@ -331,12 +338,6 @@ void Device::resetInteractiveTimout() {
   // }
 
   // here is where the magic happens, tune at your taste and enjoy your day!!
-  if (((timeinfo.tm_hour >= 18) && (timeinfo.tm_hour <= 24)) || ((timeinfo.tm_hour >=0) && (timeinfo.tm_hour <= 8))) {
-     Hardware::set_brightness(95);
-}
- else {
-     Hardware::set_brightness(45);
-  }
 
 
 
