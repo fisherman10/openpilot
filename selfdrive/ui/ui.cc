37eada6051c3824fa5393ddee7c78453fa59b3d7
fisherman10 <fisherman10@gmail.com>
2023-09-03 06:43:14 +0800

    Update ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -304,8 +304,10 @@ void Device::updateBrightness(const UIState &s) {
   // }
 
 
-	if ((timeinfo.tm_hour >= 18) && (timeinfo.tm_hour <= 8)) {
-    clipped_brightness = 5.0;
+	if ((timeinfo.tm_hour >= 18) && (timeinfo.tm_hour <= 24)) {
+        clipped_brightness = 5.0;
+        } else if ((timeinfo.tm_hour >= 0) && (timeinfo.tm_hour <= 8)) {
+        clipped_brightness = 5.0;
 	} else {
 	clipped_brightness = 100.0;	
 	}
