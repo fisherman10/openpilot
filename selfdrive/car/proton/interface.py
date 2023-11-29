b34a5cbd2879a1dc0486e189b137fe6f54f51d02
fisherman10 <fisherman10@gmail.com>
2023-11-29 19:25:58 +0800

    Update interface.py

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -36,7 +36,7 @@ class CarInterface(CarInterfaceBase):
 
     if candidate == CAR.X50:
       ret.wheelbase = 2.6
-      ret.steerRatio = 12.8
+      ret.steerRatio = 12.75
       ret.centerToFront = ret.wheelbase * 0.44
       tire_stiffness_factor = 0.9871
       ret.mass = 1370. + STD_CARGO_KG
@@ -47,8 +47,8 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [0., 25., 35., 40.]
       ret.lateralTuning.pid.kpV = [0.06, 0.14, 0.16, 0.20]
       ret.lateralTuning.pid.kiBP = [0., 20., 25., 30., 40.]
-      ret.lateralTuning.pid.kiV = [0.02, 0.01, 0.01, 0.01, 0.01]
-      ret.lateralTuning.pid.kf = 0.00014184204
+      ret.lateralTuning.pid.kiV = [0.02, 0.02, 0.02, 0.02, 0.02]
+      ret.lateralTuning.pid.kf = 0.00014184203
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
       ret.longitudinalTuning.kpV = [0, 0, 0]
