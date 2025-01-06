ead9f44be97e720eed62f8705aecb13fab2a2154
fisherman10 <fisherman10@gmail.com>
2025-01-07 00:28:15 +0800

    Update interface.py

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -24,10 +24,10 @@ class CarInterface(CarInterfaceBase):
     ret.enableApgs = False                 # advanced parking guidance system
     ret.enableDsu = False                  # driving support unit
 
-    ret.steerRateCost = 0.7                # Lateral MPC cost on steering rate, higher value = sharper turn
+    ret.steerRateCost = 0.99                # Lateral MPC cost on steering rate, higher value = sharper turn
     ret.steerLimitTimer = 0.1              # time before steerLimitAlert is issued
     ret.steerControlType = car.CarParams.SteerControlType.torque
-    ret.steerActuatorDelay = 0.30          # Steering wheel actuator delay in seconds
+    ret.steerActuatorDelay = 0.01          # Steering wheel actuator delay in seconds
 
     ret.lateralTuning.init('pid')
     ret.enableGasInterceptor = 0x201 in fingerprint[0] or 0x401 in fingerprint[0]
@@ -35,19 +35,19 @@ class CarInterface(CarInterfaceBase):
 
     if candidate == CAR.X50:
       ret.wheelbase = 2.6
-      ret.steerRatio = 15.00
+      ret.steerRatio = 18.00
       ret.centerToFront = ret.wheelbase * 0.44
       tire_stiffness_factor = 0.9871
       ret.mass = 1370. + STD_CARGO_KG
       ret.wheelSpeedFactor = 1
 
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [530]]
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [598]]  #maximum is 600 only. Any more it will disengage and reengage on its own.
 
-      ret.lateralTuning.pid.kpBP = [0., 25., 35., 40.]
-      ret.lateralTuning.pid.kpV = [0.05, 0.15, 0.15, 0.16]
-      ret.lateralTuning.pid.kiBP = [0., 20., 30.]
-      ret.lateralTuning.pid.kiV = [0.04, 0.08, 0.16]
-      ret.lateralTuning.pid.kf = 0.0000015
+      ret.lateralTuning.pid.kpBP = [0., 15., 25., 35., 40.]
+      ret.lateralTuning.pid.kpV = [0.10, 0.17, 0.21, 0.28, 0.28]
+      ret.lateralTuning.pid.kiBP = [0., 20., 25., 30., 40.]
+      ret.lateralTuning.pid.kiV = [0.01, 0.02, 0.035, 0.035, 0.035]
+      ret.lateralTuning.pid.kf = 0.00009500000
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
       ret.longitudinalTuning.kpV = [0, 0, 0]
