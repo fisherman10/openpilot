kommuai/bukapilot

Changes to allow multiple features to be set

common/features.py
@@ -22,16 +22,36 @@ class Features:
   def reset(self):
     self.p.put("FeaturesValue", b'\x00' * 8)
 
-  def set(self, feature):
-    v = int.from_bytes(self.p.get("FeaturesValue"), byteorder="little")
-    v = v | self.dict[feature]
-    self.p.put("FeaturesValue", int.to_bytes(v, byteorder="little", length=8))
-
   def set_package(self, package):
-    if package not in self.packages:
-      return
     self.reset()
-    for feature in self.packages[package]:
-      self.set(feature)
-    self.p.put("FeaturesPackage", package)
+    cleaned_package = package.replace(" ", "").replace(".", "")
 
+    total_value = 0  # To accumulate the sum of valid features
+    formatted_features = []
+    has_invalid_feature = False  # Flag to track if any feature is invalid
+    added_features = set()  # To track added features and avoid duplicates
+
+    # Split the cleaned input string by commas
+    packages_list = cleaned_package.split(',')
+
+    for pkg_feature in packages_list:
+      if pkg_feature in self.packages:
+        features_list = self.packages[pkg_feature]
+        for feature in features_list:
+          if feature != "default" and feature not in added_features:  # Skip adding "default" and duplicates
+            feature_value = self.dict[feature]
+            total_value |= feature_value  # Accumulate feature values
+            formatted_features.append(feature)
+            added_features.add(feature)  # Mark the feature as added
+      else:
+        has_invalid_feature = True  # Mark that there was an invalid feature
+
+    # Set formatted features or default if none
+    if formatted_features:
+      self.p.put("FeaturesPackageNames", ', '.join(formatted_features))
+    else:
+      self.p.put("FeaturesPackageNames", "default")
+
+    # Set the accumulated value to FeaturesValue
+    self.p.put("FeaturesValue", int.to_bytes(total_value, byteorder="little", length=8))
+    return -1 if has_invalid_feature else total_value

selfdrive/common/features.cc
@@ -1,6 +1,7 @@
 #include "selfdrive/common/features.h"
-
 #include "json11.hpp"
+#include <sstream>
+#include <set>
 
 Features::Features() {
   std::string err;
@@ -35,17 +36,51 @@ void Features::reset() {
   params.put("FeaturesValue", (const char *) &v, 8);
 }
 
-void Features::set(const std::string& feature) {
-  uint64_t v = *((uint64_t *) params.get("FeaturesValue").c_str());
-  v |= features[feature];
-  params.put("FeaturesValue", (const char *) &v, 8);
-}
-
-void Features::set_package(const std::string& package) {
+int Features::set_package(const std::string& featuresInput) {
   reset();
-  if (packages.find(package) == packages.end())
-    return;
-  for (auto const& feature : packages[package])
-    set(feature);
-  params.put("FeaturesPackage", package);
+  std::string cleaned_featuresInput = featuresInput;
+
+  cleaned_featuresInput.erase(remove_if(cleaned_featuresInput.begin(), cleaned_featuresInput.end(), [](char c) {
+    return ::isspace(c) || c == '.';
+  }), cleaned_featuresInput.end());
+
+  uint64_t total_value = 0;  // To accumulate the sum of valid features
+  std::stringstream formatted_features;
+  bool first = true;
+  bool has_invalid_feature = false; // Flag to track if any feature is invalid
+  std::set<std::string> added_features; // To track already added feature package names
+
+  std::stringstream ss(cleaned_featuresInput);
+  std::string pkg_feature;
+
+  while (std::getline(ss, pkg_feature, ',')) {
+    if (packages.find(pkg_feature) != packages.end()) {
+      // Check if pkg_feature is already added
+      if (added_features.find(pkg_feature) == added_features.end()) {
+        added_features.insert(pkg_feature);  // Mark pkg_feature as added
+        const auto& features_list = packages[pkg_feature];
+        for (const auto& feature : features_list) {
+          uint64_t feature_value = features[feature];
+          total_value |= feature_value;
+        }
+        if (pkg_feature != "default") {
+          if (!first) {
+            formatted_features << ", ";
+          }
+          formatted_features << pkg_feature;
+          first = false;
+        }
+      }
+    } else {
+      has_invalid_feature = true;
+    }
+  }
+
+  if (formatted_features.str().empty()) {
+    formatted_features.str("default");
+  }
+
+  params.put("FeaturesValue", (const char*)&total_value, sizeof(total_value)); // Set the accumulated value to FeaturesValue
+  params.put("FeaturesPackageNames", formatted_features.str()); // Set the final formatted string of valid features to Params
+  return has_invalid_feature ? -1 : static_cast<int>(total_value); // Return -1 if any feature was invalid; otherwise return the total as int
 }

selfdrive/common/features.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <set>
 
 #include "selfdrive/common/params.h"
 
@@ -13,8 +14,7 @@ public:
   void clear(const std::string& feature);
   bool has(const std::string& feature);
   void reset();
-  void set(const std::string& feature);
-  void set_package(const std::string& package);
+  int set_package(const std::string& featuresInput);
 
   Params params;
   std::map<std::string, uint64_t> features;

selfdrive/manager/manager.py
@@ -137,7 +137,8 @@ def manager_init() -> None:
       "version": 4,
       }
   cur_dict = params.get("FeaturesDict")
-  if cur_dict is None or (json.loads(cur_dict)["version"] < new_dict["version"]):
+  if cur_dict is None or (json.loads(cur_dict)["version"] < new_dict["version"]) \
+      or params.get("FeaturesPackageNames") is None:
     params.put("FeaturesDict", json.dumps(new_dict))
     Features().set_package("default")
 

selfdrive/ui/qt/offroad/k_settings.h
@@ -85,17 +85,26 @@ class FeaturesControl : public ButtonControl {
   Q_OBJECT
 
 public:
-  FeaturesControl() : ButtonControl("Features Package", "SET", "Warning: Only use under guidance of a support staff.") {
+  FeaturesControl() : ButtonControl("Features Package", "EDIT", "Warning: Only use under guidance of a support staff.") {
     package_label = new QLabel();
     package_label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
     package_label->setStyleSheet("color: #aaaaaa");
-    package_label->setText(Params().get("FeaturesPackage").c_str());
+    setElidedText(package_label, Params().get("FeaturesPackageNames").c_str());
     hlayout->insertWidget(1, package_label);
     connect(this, &ButtonControl::clicked, [=] {
-      QString package = InputDialog::getText("Enter Feature Package Name", this);
-      if (package.length() > 0) {
-        Features().set_package(package.toStdString());
-        package_label->setText(Params().get("FeaturesPackage").c_str());
+      InputDialog dialog("Enter Feature Package Names", this,
+        "Feature package names are separated by commas ( \u2009<b>,</b>\u2009 ).<br>Empty to use the default feature package.");
+      dialog.setMinLength(0);
+      QString currentFeatures = Params().get("FeaturesPackageNames").c_str();
+      if (currentFeatures != "default") dialog.updateDefaultText(currentFeatures + ", ");
+      int ret = dialog.exec();
+      if (ret == QDialog::Accepted) {
+        QString packages = dialog.text();
+        int result = Features().set_package(packages.toStdString());
+        setElidedText(package_label, Params().get("FeaturesPackageNames").c_str());
+        if (result == -1) {
+          ConfirmationDialog::alert("\nSome feature package names\nare invalid and were not added.", this);
+        }
       }
     });
   }
@@ -112,7 +121,7 @@ public:
     selection_label = new QLabel();
     selection_label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
     selection_label->setStyleSheet("color: #aaaaaa");
-    selection_label->setText(Params().get("FixFingerprint").c_str());
+    setElidedText(selection_label, Params().get("FixFingerprint").c_str());
     hlayout->insertWidget(1, selection_label);
     connect(this, &ButtonControl::clicked, [=] {
       QString package = InputDialog::getText("Enter Car Model", this);
@@ -122,7 +131,7 @@ public:
       else {
         Params().put("FixFingerprint", "");
       }
-      selection_label->setText(Params().get("FixFingerprint").c_str());
+      setElidedText(selection_label, Params().get("FixFingerprint").c_str());
     });
   }
 
@@ -149,7 +158,7 @@ public:
     // Display current branch name
     std::string currentBranchName = util::check_output("git symbolic-ref --short HEAD");
     currentBranchName.pop_back(); // Remove the last character (line feed)
-    selection_label->setText(QString::fromStdString(currentBranchName));
+    setElidedText(selection_label, QString::fromStdString(currentBranchName));
     system(setUpstream(currentBranchName).c_str());
     hlayout->insertWidget(1, selection_label);
     connect(this, &ButtonControl::clicked, [=] {
@@ -169,7 +178,7 @@ public:
           int branchChanged = system(changeBranchConfigReboot.c_str());
 		      if (branchChanged != 0) { // If branch not found (error/fatal)
 			      QString failedPrompt = QString::fromStdString("Branch " + branchName + " not found.\n\nPlease make sure the branch name is correct and the device is connected to the Internet.");
-		        (ConfirmationDialog::alert(failedPrompt, this));
+		        ConfirmationDialog::alert(failedPrompt, this);
           }
         }
       }

fisherman10/openpilot

common/features.py
@@ -22,16 +22,36 @@ class Features:
   def reset(self):
     self.p.put("FeaturesValue", b'\x00' * 8)
 
-  def set(self, feature):
-    v = int.from_bytes(self.p.get("FeaturesValue"), byteorder="little")
-    v = v | self.dict[feature]
-    self.p.put("FeaturesValue", int.to_bytes(v, byteorder="little", length=8))
-
   def set_package(self, package):
-    if package not in self.packages:
-      return
     self.reset()
-    for feature in self.packages[package]:
-      self.set(feature)
-    self.p.put("FeaturesPackage", package)
+    cleaned_package = package.replace(" ", "").replace(".", "")
 
+    total_value = 0  # To accumulate the sum of valid features
+    formatted_features = []
+    has_invalid_feature = False  # Flag to track if any feature is invalid
+    added_features = set()  # To track added features and avoid duplicates
+
+    # Split the cleaned input string by commas
+    packages_list = cleaned_package.split(',')
+
+    for pkg_feature in packages_list:
+      if pkg_feature in self.packages:
+        features_list = self.packages[pkg_feature]
+        for feature in features_list:
+          if feature != "default" and feature not in added_features:  # Skip adding "default" and duplicates
+            feature_value = self.dict[feature]
+            total_value |= feature_value  # Accumulate feature values
+            formatted_features.append(feature)
+            added_features.add(feature)  # Mark the feature as added
+      else:
+        has_invalid_feature = True  # Mark that there was an invalid feature
+
+    # Set formatted features or default if none
+    if formatted_features:
+      self.p.put("FeaturesPackage", ', '.join(formatted_features))
+    else:
+      self.p.put("FeaturesPackage", "default")
+
+    # Set the accumulated value to FeaturesValue
+    self.p.put("FeaturesValue", int.to_bytes(total_value, byteorder="little", length=8))
+    return -1 if has_invalid_feature else total_value

selfdrive/common/features.cc
@@ -1,6 +1,7 @@
 #include "selfdrive/common/features.h"
-
 #include "json11.hpp"
+#include <sstream>
+#include <set>
 
 Features::Features() {
   std::string err;
@@ -35,17 +36,51 @@ void Features::reset() {
   params.put("FeaturesValue", (const char *) &v, 8);
 }
 
-void Features::set(const std::string& feature) {
-  uint64_t v = *((uint64_t *) params.get("FeaturesValue").c_str());
-  v |= features[feature];
-  params.put("FeaturesValue", (const char *) &v, 8);
-}
-
-void Features::set_package(const std::string& package) {
+int Features::set_package(const std::string& package) {
   reset();
-  if (packages.find(package) == packages.end())
-    return;
-  for (auto const& feature : packages[package])
-    set(feature);
-  params.put("FeaturesPackage", package);
+  std::string cleaned_package = package;
+
+  cleaned_package.erase(remove_if(cleaned_package.begin(), cleaned_package.end(), [](char c) {
+    return ::isspace(c) || c == '.';
+  }), cleaned_package.end());
+
+  uint64_t total_value = 0;  // To accumulate the sum of valid features
+  std::stringstream formatted_features;
+  bool first = true;
+  bool has_invalid_feature = false; // Flag to track if any feature is invalid
+  std::set<std::string> added_features; // To track already added feature package names
+
+  std::stringstream ss(cleaned_package);
+  std::string pkg_feature;
+
+  while (std::getline(ss, pkg_feature, ',')) {
+    if (packages.find(pkg_feature) != packages.end()) {
+      // Check if pkg_feature is already added
+      if (added_features.find(pkg_feature) == added_features.end()) {
+        added_features.insert(pkg_feature);  // Mark pkg_feature as added
+        const auto& features_list = packages[pkg_feature];
+        for (const auto& feature : features_list) {
+          uint64_t feature_value = features[feature];
+          total_value |= feature_value;
+        }
+        if (pkg_feature != "default") {
+          if (!first) {
+            formatted_features << ", ";
+          }
+          formatted_features << pkg_feature;
+          first = false;
+        }
+      }
+    } else {
+      has_invalid_feature = true;
+    }
+  }
+
+  if (formatted_features.str().empty()) {
+    formatted_features.str("default");
+  }
+
+  params.put("FeaturesValue", (const char*)&total_value, sizeof(total_value)); // Set the accumulated value to FeaturesValue
+  params.put("FeaturesPackage", formatted_features.str()); // Set the final formatted string of valid features to Params
+  return has_invalid_feature ? -1 : static_cast<int>(total_value); // Return -1 if any feature was invalid; otherwise return the total as int
 }

selfdrive/common/features.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <set>
 
 #include "selfdrive/common/params.h"
 
@@ -13,8 +14,7 @@ public:
   void clear(const std::string& feature);
   bool has(const std::string& feature);
   void reset();
-  void set(const std::string& feature);
-  void set_package(const std::string& package);
+  int set_package(const std::string& package);
 
   Params params;
   std::map<std::string, uint64_t> features;

selfdrive/manager/manager.py
@@ -137,7 +137,8 @@ def manager_init() -> None:
       "version": 4,
       }
   cur_dict = params.get("FeaturesDict")
-  if cur_dict is None or (json.loads(cur_dict)["version"] < new_dict["version"]):
+  if cur_dict is None or (json.loads(cur_dict)["version"] < new_dict["version"]) \
+      or params.get("FeaturesPackage") is None:
     params.put("FeaturesDict", json.dumps(new_dict))
     Features().set_package("default")
 

selfdrive/ui/qt/offroad/k_settings.h
@@ -85,17 +85,26 @@ class FeaturesControl : public ButtonControl {
   Q_OBJECT
 
 public:
-  FeaturesControl() : ButtonControl("Features Package", "SET", "Warning: Only use under guidance of a support staff.") {
+  FeaturesControl() : ButtonControl("Features Package", "EDIT", "Warning: Only use under guidance of a support staff.") {
     package_label = new QLabel();
     package_label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
     package_label->setStyleSheet("color: #aaaaaa");
-    package_label->setText(Params().get("FeaturesPackage").c_str());
+    setElidedText(package_label, Params().get("FeaturesPackage").c_str());
     hlayout->insertWidget(1, package_label);
     connect(this, &ButtonControl::clicked, [=] {
-      QString package = InputDialog::getText("Enter Feature Package Name", this);
-      if (package.length() > 0) {
-        Features().set_package(package.toStdString());
-        package_label->setText(Params().get("FeaturesPackage").c_str());
+      InputDialog dialog("Enter Feature Package Names", this,
+        "Feature package names are separated by commas ( \u2009<b>,</b>\u2009 ).<br>Empty to use the default feature package.");
+      dialog.setMinLength(0);
+      QString currentFeatures = Params().get("FeaturesPackage").c_str();
+      if (currentFeatures != "default") dialog.updateDefaultText(currentFeatures + ", ");
+      int ret = dialog.exec();
+      if (ret == QDialog::Accepted) {
+        QString packages = dialog.text();
+        int result = Features().set_package(packages.toStdString());
+        setElidedText(package_label, Params().get("FeaturesPackage").c_str());
+        if (result == -1) {
+          ConfirmationDialog::alert("\nSome feature package names\nare invalid and were not added.", this);
+        }
       }
     });
   }
@@ -112,7 +121,7 @@ public:
     selection_label = new QLabel();
     selection_label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
     selection_label->setStyleSheet("color: #aaaaaa");
-    selection_label->setText(Params().get("FixFingerprint").c_str());
+    setElidedText(selection_label, Params().get("FixFingerprint").c_str());
     hlayout->insertWidget(1, selection_label);
     connect(this, &ButtonControl::clicked, [=] {
       QString package = InputDialog::getText("Enter Car Model", this);
@@ -122,7 +131,7 @@ public:
       else {
         Params().put("FixFingerprint", "");
       }
-      selection_label->setText(Params().get("FixFingerprint").c_str());
+      setElidedText(selection_label, Params().get("FixFingerprint").c_str());
     });
   }
 
@@ -149,7 +158,7 @@ public:
     // Display current branch name
     std::string currentBranchName = util::check_output("git symbolic-ref --short HEAD");
     currentBranchName.pop_back(); // Remove the last character (line feed)
-    selection_label->setText(QString::fromStdString(currentBranchName));
+    setElidedText(selection_label, QString::fromStdString(currentBranchName));
     system(setUpstream(currentBranchName).c_str());
     hlayout->insertWidget(1, selection_label);
     connect(this, &ButtonControl::clicked, [=] {
@@ -169,7 +178,7 @@ public:
           int branchChanged = system(changeBranchConfigReboot.c_str());
 		      if (branchChanged != 0) { // If branch not found (error/fatal)
 			      QString failedPrompt = QString::fromStdString("Branch " + branchName + " not found.\n\nPlease make sure the branch name is correct and the device is connected to the Internet.");
-		        (ConfirmationDialog::alert(failedPrompt, this));
+		        ConfirmationDialog::alert(failedPrompt, this);
           }
         }
       }