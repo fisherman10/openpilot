3f8698833f36ace28f5aeca9c5c99f0f41a1932c
fisherman10 <fisherman10@gmail.com>
2025-04-27 23:08:27 +0800

    Adjust driver override threshold

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -46,7 +46,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [0., 15., 25., 35., 40.]
       ret.lateralTuning.pid.kpV = [0.09, 0.16, 0.18, 0.19, 0.20]
       ret.lateralTuning.pid.kiBP = [0., 20., 25., 30., 40.]
-      ret.lateralTuning.pid.kiV = [0.05, 0.05, 0.05, 0.05, 0.05]
+      ret.lateralTuning.pid.kiV = [0.08, 0.08, 0.06, 0.06, 0.06]
       ret.lateralTuning.pid.kf = 0.000094304000000000
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
