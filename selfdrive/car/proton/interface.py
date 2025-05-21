f6427b606602c29c61afcf41dcc56ed0646247ad
fisherman10 <fisherman10@gmail.com>
2025-05-21 19:00:13 +0800

    Update interface.py

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -25,7 +25,7 @@ class CarInterface(CarInterfaceBase):
     ret.enableDsu = False                  # driving support unit
 
     ret.steerRateCost = 0.99              #steerRateCost lower is more reactive and higher is slower to react. Adjusting steerRateCost will affect how eager the car is to make sudden direction changes. Lower cost == more eager
-    ret.steerLimitTimer = 0.05            # time before steerLimitAlert is issued
+    ret.steerLimitTimer = 0.01            # time before steerLimitAlert is issued
     ret.steerControlType = car.CarParams.SteerControlType.torque
     ret.steerActuatorDelay = 0.00         # Steering wheel actuator delay in seconds
 
@@ -46,8 +46,8 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [0., 15., 25., 35., 40.]
       ret.lateralTuning.pid.kpV = [0.05, 0.16, 0.16, 0.18, 0.18]
       ret.lateralTuning.pid.kiBP = [0., 20., 25., 30., 40.]
-      ret.lateralTuning.pid.kiV = [0.04, 0.04, 0.02, 0.02, 0.02]
-      ret.lateralTuning.pid.kf = 0.0000940000000
+      ret.lateralTuning.pid.kiV = [0.04, 0.04, 0.04, 0.04, 0.04]
+      ret.lateralTuning.pid.kf = 0.00009350000000
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
       ret.longitudinalTuning.kpV = [0, 0, 0]
