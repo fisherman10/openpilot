4f380d11cb24cd4551c9e41a9f449c6ae11bd74e
fisherman10 <fisherman10@gmail.com>
2025-09-09 15:00:54 +0800

    Update interface.py

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -47,7 +47,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV = [0.08, 0.14, 0.16, 0.16, 0.16]
       ret.lateralTuning.pid.kiBP = [0., 20., 25., 30., 40.]
       ret.lateralTuning.pid.kiV = [0.03, 0.03, 0.03, 0.03, 0.03]
-      ret.lateralTuning.pid.kf = 0.0000900000000000
+      ret.lateralTuning.pid.kf = 0.00009050000000000
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
       ret.longitudinalTuning.kpV = [0, 0, 0]
