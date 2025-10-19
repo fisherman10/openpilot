654244a0e0f816cb64548aa1e56b906ddf80d3e1
fisherman10 <fisherman10@gmail.com>
2025-10-20 01:09:17 +0800

    Update interface.py

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -24,8 +24,8 @@ class CarInterface(CarInterfaceBase):
     ret.enableApgs = False                 # advanced parking guidance system
     ret.enableDsu = False                  # driving support unit
 
-    ret.steerRateCost = 0.99              #steerRateCost lower is more reactive and higher is slower to react. Adjusting steerRateCost will affect how eager the car is to make sudden direction changes. Lower cost == more eager
-    ret.steerLimitTimer = 0.01            # time before steerLimitAlert is issued
+    ret.steerRateCost = 1.5              #steerRateCost lower is more reactive and higher is slower to react. Adjusting steerRateCost will affect how eager the car is to make sudden direction changes. Lower cost == more eager
+    ret.steerLimitTimer = 0.001            # time before steerLimitAlert is issued
     ret.steerControlType = car.CarParams.SteerControlType.torque
     ret.steerActuatorDelay = 0.01         # Steering wheel actuator delay in seconds
 
@@ -46,8 +46,8 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [0., 15., 25., 35., 40.]
       ret.lateralTuning.pid.kpV = [0.08, 0.12, 0.14, 0.16, 0.16]
       ret.lateralTuning.pid.kiBP = [0., 20., 25., 30., 40.]
-      ret.lateralTuning.pid.kiV = [0.01, 0.01, 0.01, 0.01, 0.01]
-      ret.lateralTuning.pid.kf = 0.0000922000000000
+      ret.lateralTuning.pid.kiV = [0.005, 0.005, 0.005, 0.005, 0.005]
+      ret.lateralTuning.pid.kf = 0.0000920000000000
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
       ret.longitudinalTuning.kpV = [0, 0, 0]
