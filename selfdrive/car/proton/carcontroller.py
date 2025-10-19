3ed2dad646b48d49324e23b99e91da1b56e5dc1c
fisherman10 <fisherman10@gmail.com>
2025-10-19 11:55:37 +0800

    Update carcontroller.py

--- selfdrive/car/proton/carcontroller.py
+++ selfdrive/car/proton/carcontroller.py
@@ -42,8 +42,8 @@ class CarControllerParams():
 
     # for torque limit calculation
 
-    self.STEER_DELTA_UP = 6                      # torque increase per refresh, 0.8s to max
-    self.STEER_DELTA_DOWN = 10                    # torque decrease per refresh
+    self.STEER_DELTA_UP = 5                      # torque increase per refresh, 0.8s to max
+    self.STEER_DELTA_DOWN = 8                    # torque decrease per refresh
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
