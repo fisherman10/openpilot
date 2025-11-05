5baaa966334a0daf4d45e9e64f560862c439e47a
fisherman10 <fisherman10@gmail.com>
2025-11-05 18:52:40 +0800

    Update interface.py

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -44,10 +44,10 @@ class CarInterface(CarInterfaceBase):
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [600]]  #maximum is 600 only. Any more it will disengage and reengage on its own.
 
       ret.lateralTuning.pid.kpBP = [0., 15., 25., 35., 40.]
-      ret.lateralTuning.pid.kpV = [0.04, 0.10, 0.13, 0.16, 0.16]
+      ret.lateralTuning.pid.kpV = [0.04, 0.08, 0.12, 0.15, 0.15]
       ret.lateralTuning.pid.kiBP = [0., 20., 25., 30., 40.]
-      ret.lateralTuning.pid.kiV = [0.03, 0.03, 0.02, 0.01, 0.01]
-      ret.lateralTuning.pid.kf = 0.0000910200000000
+      ret.lateralTuning.pid.kiV = [0.06, 0.06, 0.04, 0.03, 0.02]
+      ret.lateralTuning.pid.kf = 0.0000910300000000
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
       ret.longitudinalTuning.kpV = [0, 0, 0]
