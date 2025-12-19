da71efcf4cd687166a32f7e09a8badb14a2bc977
fisherman10 <fisherman10@gmail.com>
2025-12-19 19:29:36 +0800

    Adjust torque limits for steering control

--- selfdrive/car/proton/carcontroller.py
+++ selfdrive/car/proton/carcontroller.py
@@ -42,8 +42,8 @@ class CarControllerParams():
 
     # for torque limit calculation
 
-    self.STEER_DELTA_UP = 16                      # torque increase per refresh, 0.8s to max
-    self.STEER_DELTA_DOWN = 18                    # torque decrease per refresh
+    self.STEER_DELTA_UP = 20                      # torque increase per refresh, 0.8s to max
+    self.STEER_DELTA_DOWN = 15                    # torque decrease per refresh
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
