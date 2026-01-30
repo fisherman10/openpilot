85e1239a87d533a7bc14d32980e977b45755c0c6
fisherman10 <fisherman10@gmail.com>
2026-01-30 20:27:08 +0800

    Update carcontroller.py

--- selfdrive/car/proton/carcontroller.py
+++ selfdrive/car/proton/carcontroller.py
@@ -17,8 +17,8 @@ RES_LEN = 2 # Press resume for 2 frames
 def apply_proton_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, LIMITS):
 
   # limits due to driver torque
-  driver_max_torque = LIMITS.STEER_MAX + driver_torque * 10
-  driver_min_torque = -LIMITS.STEER_MAX + driver_torque * 10
+  driver_max_torque = LIMITS.STEER_MAX + driver_torque * 9
+  driver_min_torque = -LIMITS.STEER_MAX + driver_torque * 9
   max_steer_allowed = max(min(LIMITS.STEER_MAX, driver_max_torque), 0)
   min_steer_allowed = min(max(-LIMITS.STEER_MAX, driver_min_torque), 0)
   apply_torque = clip(apply_torque, min_steer_allowed, max_steer_allowed)
@@ -42,7 +42,7 @@ class CarControllerParams():
 
     # for torque limit calculation
 
-    self.STEER_DELTA_UP = 22                      # torque increase per refresh, 0.8s to max
+    self.STEER_DELTA_UP = 20                      # torque increase per refresh, 0.8s to max
     self.STEER_DELTA_DOWN = 23                    # torque decrease per refresh
 
 class CarController():
