c17c63964d898bc7a8eebe916d84d167ddc8d0b8
fisherman10 <fisherman10@gmail.com>
2026-02-08 10:32:21 +0800

    Adjust kiV parameter in lateral tuning

--- selfdrive/car/proton/interface.py
+++ selfdrive/car/proton/interface.py
@@ -46,7 +46,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [0., 17., 25., 35., 40.]
       ret.lateralTuning.pid.kpV = [0.05, 0.12, 0.14, 0.16, 0.17]
       ret.lateralTuning.pid.kiBP = [0., 40.]
-      ret.lateralTuning.pid.kiV = [0.03, 0.04]
+      ret.lateralTuning.pid.kiV = [0.01, 0.04]
       ret.lateralTuning.pid.kf = 0.0000937834110000000000
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
